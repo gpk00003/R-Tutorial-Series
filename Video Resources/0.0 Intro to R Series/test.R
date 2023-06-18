fc9e50f8b82926d9c846ec0ecffa2803330182df
gpk00003 <122639490+gpk00003@users.noreply.github.com>
2023-06-18 15:11:57 -0400

    added better example files

---
+++ Video Resources/0.0 Intro to R Series/test.R
@@ -0,0 +1,11 @@
+#this is coding
+
+standard_american <- "hello_world!"
+british <- "'ello_gov'na!"
+country <- "howdy y'all!"
+cat <- "meow meow!"
+
+print(standard_american)
+print(british)
+print(country)
+print(cat)
